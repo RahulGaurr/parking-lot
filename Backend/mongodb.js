1197982c02e48114ecb2d691c0c08cc8bac8b12f
Rahul Gaur <97507317+RahulGaurr@users.noreply.github.com>
2022-06-05 00:52:58 +0530

    Delete Backend directory

--- Backend/mongodb.js
+++
@@ -1,8 +0,0 @@
-const http = require("http")
-const connectmongodb = require("./mongodb")
-const app = require("./app")
-
-http.createServer(app).listen(8001, () => {
-    connectmongodb
-    console.log("Your app is running on localhost:8001")
-})
